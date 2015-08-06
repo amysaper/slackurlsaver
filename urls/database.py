8a06e707fefb650b4e461def83016062584561b8
amysaper <amycsaper@gmail.com>
2015-08-06 00:31:33 +0000

    database update

--- urls/database.py
+++ urls/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-from tuneful import app
+from urls import app
 
 engine = create_engine(app.config["DATABASE_URI"])
 Base = declarative_base()
