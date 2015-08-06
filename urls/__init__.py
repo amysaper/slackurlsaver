b2ac85674f27ee8b8da5cd50b922a05787e06a88
amysaper <amycsaper@gmail.com>
2015-08-06 00:27:31 +0000

    added database file

--- urls/__init__.py
+++ urls/__init__.py
@@ -7,7 +7,7 @@ config_path = os.environ.get("CONFIG_PATH", "urls.config.DevelopmentConfig")
 app.config.from_object(config_path)
 
 import api
-import views
+#import views
 
 from database import Base, engine
 Base.metadata.create_all(engine)
