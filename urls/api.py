0e1f3d502832e98232b445ed088897cb2e05acc5
amysaper <amycsaper@gmail.com>
2015-08-08 01:39:40 +0000

    fixed flask app

--- urls/api.py
+++ urls/api.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 from os import environ
+from . import app
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
 @app.route("/")
 @app.route("/save_url", methods=["POST"])
