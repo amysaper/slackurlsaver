1e99501adee0792c2810a184eeb3173362b850fb
amysaper <amycsaper@gmail.com>
2015-09-05 01:22:09 +0000

    added a database

--- urls/api.py
+++ urls/api.py
@@ -2,14 +2,31 @@ from flask import Flask, render_template
 from os import environ
 from . import app
 
+from .database import URL, session
+
+from flask import request
+
 #app = Flask(__name__)
 
 @app.route("/")
 @app.route("/save_url", methods=["POST"])
-def say_hi():
-    print "URL Saved! Type /urls to retreive the list of all saved URLS."
-    print (app.config["DATABASE_URI"])
-    return "Woo hoo! URL saved!"
+
+def save_url():
+    return repr(request.data) 
+    #session.add(url)
+    session.commit()
+    
+    
+    #return "URL saved! Type /urls to retrieve a list of all saved URLs."
+    
+
+#def say_hi():
+    #print "URL Saved! Type /urls to retreive the list of all saved URLS."
+    #print (app.config["DATABASE_URI"])
+    
+    #print request.data or request.form
+    
+   # return "Woo hoo! URL saved!"
 
     
 if __name__ == "__main__":
--- urls/database.py
+++ urls/database.py
@@ -8,3 +8,22 @@ engine = create_engine(app.config["DATABASE_URI"])
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
+
+import datetime
+
+from sqlalchemy import Column, Integer, String, Text, DateTime
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+class URL(Base):
+    __tablename__ = "urls"
+
+    id = Column(Integer, primary_key=True)
+    url = Column(Text)
+    savedate = Column(DateTime, default=datetime.datetime.now)
+    user= Column(Text)
+
+
+
+Base.metadata.create_all(engine)
